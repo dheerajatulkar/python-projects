8d135d632c59b7012ee48a091ebb1d6c65d81257
Dheeraj Atulkar <goldiatulkar@gmail.com>
2024-01-13 10:56:32 +0530

    Create listDictionaryPractice.py

---
+++ listDictionaryPractice.py
@@ -0,0 +1,19 @@
+num = int(input("Enter a number of student: "))
+student_data = []
+for i in range(num):
+  data = {}
+  marks=[]
+  name = input("Enter name of student: ")
+  roll = int(input("Enter roll number: "))
+  for j in range(5):
+    mark = int(input(f"Enter the marks of {j+1}: "))
+    marks.append(mark)
+  data["name"] = name
+  data['roll']=roll
+  data["marks"]=marks  
+  student_data.append(data)
+
+for stu in student_data:
+  print(f"the name is {stu['name']} and the roll number is {stu['roll']}")
+  print(f"the total marks are {sum(stu['marks'])}")
+  
